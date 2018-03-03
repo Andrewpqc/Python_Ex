b5d34f901859011a9b232e0390502f234d3d3357
Andrewpqc <m13636038496@163.com>
2018-03-03 23:02:47 +0800

    add some exercises for python

---
+++ python-algorithom/binary-search.py
@@ -0,0 +1,26 @@
+"""
+二分查找
+"""
+def binary_search(list,item):
+    """
+    list:有序列表(从小到大排列)
+    item:需要查找的元素
+    """
+    low=0
+    high=len(list)-1
+
+    while low<high:
+        mid=(low+high)//2
+        if item==list[mid]:
+            return mid
+        elif item>list[mid]: #查找值位于mid右边
+            low=mid+1
+        else:
+            high=mid-1      #查找值位于mid左边
+        return None         #没找到则返回None　　
+
+
+if __name__=="__main__":
+    l=[1,2,3,4,5,6]
+    print(binary_search(l,3))
+    print(binary_search(l,7))
