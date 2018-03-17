4246c3e921ef318e6af0495421916fe5db22793b
Andrewpqc <m13636038496@163.com>
2018-03-17 21:42:59 +0800

    add celery worker and beat

--- celery_demo/client.py
+++
@@ -1,8 +0,0 @@
-from celery_app.task1 import add
-from celery_app.task2 import sub
-
-a=add.apply_async(args=[2, 8])        # 也可用 task1.add.delay(2, 8)
-b=sub.apply_async(args=[3, 7])   # 也可用 task2.multiply.delay(3, 7)
-print(a.get())
-print(b.get())
-print('hello world')
