ba64c656740ec374e3ac1fe833e789bcd25e4fc9
Andrewpqc <m13636038496@163.com>
2018-04-07 04:05:06 +0800

    add something

---
+++ mass/baidu_zhishu.py
@@ -0,0 +1,3 @@
+with open("/home/andrew/下载/WLvHq+dH.bin","rb") as f:
+    s=f.read()
+    print(s)
---
+++ mass/jisuan.py
@@ -0,0 +1,47 @@
+#火
+print("火：")
+print("x:")
+for x in [2,18.5,38.6,48.9]:
+    print(45.1/x)
+print("y:")
+for y in [17.5,12.3,2.5,15.1]:
+    print(25.5/y)
+
+print()
+#花A
+print("花A:")
+print("x:")
+for x in [4.8,7.8,14.7,26.1,37.4,41.8]:
+    print(45.1 / x)
+
+print("y:")
+for y in [23.1,5.6,0.4,13.8,5.1,18.1]:
+    print(25.5 / y)
+
+
+print()
+#花B
+print("花B:")
+print("x:")
+for x in [5.15,14.2,29.8,39.6]:
+    print(45.1 / x)
+
+print("y:")
+for y in [17.9,8.7,11.9,0.6]:
+    print(25.5 / y)
+
+print()
+
+#水
+print("水:")
+print("x:")
+for x in [14.6,25.2,32.9,14.3]:
+    print(45.1 / x)
+
+print("y:")
+for y in [14.2,8.3,5.1,10.8]:
+    print(25.5 / y)
+print()
+
+
+
